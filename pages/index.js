a96601504f2e66fbb8e4e56c1bd308fdddca3489
tirthrdesai05@gmail.com <tirthrdesai05@gmail.com>
2024-02-20 02:30:02 -0500

    Responsivenss

--- pages/index.js
+++ pages/index.js
@@ -5,30 +5,19 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className=' flex flex-row flex-wrap items-center justify-center h-[100vh] overflow-hidden p-2 gap-2 bg-slate-600'>
-      <div className=' rounded-lg w-[30%] h-[250px] flex flex-col items-center justify-center bg-slate-300 border-2 border-black '>
+    <div className=' flex flex-row flex-wrap items-center justify-center h-[100%] min-h-[100vh] p-2 gap-2 bg-slate-600'>
+      <div className=' rounded-lg w-[30%] h-[250px] flex flex-col flex-grow basis-48 items-center justify-center bg-slate-300 border-2 border-black '>
         <Tscore />
       </div>
 
-      <div className=' rounded-lg w-[30%] h-[250px] flex flex-col items-center justify-center bg-slate-300 border-2 border-black'>
+      <div className=' rounded-lg w-[30%] h-[250px] flex flex-col flex-grow basis-48 items-center justify-center bg-slate-300 border-2 border-black'>
         <Zscore />
       </div>
 
-      <div className=' rounded-lg w-[30%] h-[250px] flex flex-col items-center justify-center bg-slate-300 border-2 border-black'>
+      <div className=' rounded-lg w-[30%] h-[250px] flex flex-col flex-grow basis-48 items-center justify-center bg-slate-300 border-2 border-black'>
         <Chisquarescore />
       </div>
 
-      <div className=' rounded-lg w-[30%] h-[250px] flex flex-col items-center justify-center bg-slate-300 border-2 border-black '>
-        <h1>Home</h1>
-      </div>
-
-      <div className=' rounded-lg w-[30%] h-[250px] flex flex-col items-center justify-center bg-slate-300 border-2 border-black'>
-        <h1>Home</h1>
-      </div>
-
-      <div className=' rounded-lg w-[30%] h-[250px] flex flex-col items-center justify-center bg-slate-300 border-2 border-black'>
-        <h1>Home</h1>
-      </div>
     </div>
   )
 }
